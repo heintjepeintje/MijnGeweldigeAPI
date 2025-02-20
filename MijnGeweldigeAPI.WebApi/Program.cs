4ac49ef3fe6a394be2acf562dca82cee0e0c8df1
heintjepeintje <117925862+heintjepeintje@users.noreply.github.com>
2025-02-20 10:56:27 +0100

    added text to api

--- MijnGeweldigeAPI.WebApi/Program.cs
+++ MijnGeweldigeAPI.WebApi/Program.cs
@@ -3,14 +3,10 @@ using MijnGeweldigeAPI.WebApi.Controllers;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var sqlConnectionString = builder.Configuration["SqlConnectionString"];
-Console.WriteLine(sqlConnectionString);
+
+var sqlConnectionString = builder.Configuration.GetValue<string>("SqlConnectionString");
+var sqlConnectionStringFound = !string.IsNullOrWhiteSpace(sqlConnectionString);
 builder.Services.AddTransient(o => new EnvironmentRepository(sqlConnectionString));
-Environment2D environment = new("Mijn geweldige environment", 100, 200);
-
-EnvironmentRepository repository = new EnvironmentRepository(sqlConnectionString);
-await repository.InsertAsync(environment);
-
 // Add services to the container.
 
 builder.Services.AddControllers();
@@ -19,6 +15,7 @@ builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
+app.MapGet("/", () => $"The API is up . Connection string found: {(sqlConnectionStringFound ? "" : "")}");
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -31,6 +28,5 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
-
 
 app.Run();
